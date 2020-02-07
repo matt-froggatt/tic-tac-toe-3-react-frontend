27286618dba3643ed2a193b4104a095129821fd4
Matthew Froggatt <mattfr@live.ca>
2020-02-06 19:56:15 -0500

    Changed directory structure to better suit project.

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Board msg="Hi World"/>
+  </div>
+</template>
+
+<script>
+import Board from './components/Board.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Board
+  }
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: white;
+  min-height: 100vh;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Octothorpe.vue
@@ -0,0 +1,48 @@
+<template>
+  <table class="octothorpe">
+    <tr>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+    <tr>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+    <tr>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'Octothorpe',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  padding: 100px;
+}
+h3 {
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
