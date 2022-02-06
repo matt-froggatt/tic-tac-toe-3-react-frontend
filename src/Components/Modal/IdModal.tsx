2026eb00e27d92acca9f02721a28c38bd30ff204
Matthew John Froggatt <mattfr@live.ca>
2022-02-05 22:32:28 -0500

    Refactor to use Ramda

--- src/Components/Modal/IdModal.tsx
+++ src/Components/Modal/IdModal.tsx
@@ -15,24 +15,29 @@ const IdModal: React.FC<IDModalProps> = ({id, onIdSubmit, gameStarted}) => {
 
     useEffect(() => R.pipe(R.isNil, setIsLoading)(id), [id])
 
-    return (gameStarted ? null :
-        <StyledModal>
-            <div className="flex flex-col">
-                <div className="pb-2">
-                    <p>Your ID is {isLoading? 'loading...' : id}</p>
-                    <p>Enter a friend's ID below to play!</p>
+    return R.ifElse(
+        R.always(gameStarted),
+        R.always(null),
+        ({id, onIdSubmit}: IDModalProps) => (
+            <StyledModal>
+                <div className="flex flex-col">
+                    <div className="pb-2">
+                        <p>Your ID is {isLoading ? 'loading...' : id}</p>
+                        <p>Enter a friend's ID below to play!</p>
+                    </div>
+                    <div className="flex flex-row justify-between">
+                        <input
+                            className='border-b-2 border-black w-3/4'
+                            placeholder="Enter friend's ID here"
+                            type="number"
+                            onChange={R.pipe(R.pathOr('err', ['target', 'value']), parseInt, setEnteredValue)}
+                        />
+                        <GoodButton onClick={() => onIdSubmit(enteredValue)}>Go!</GoodButton>
+                    </div>
                 </div>
-                <div className="flex flex-row justify-between">
-                    <input
-                        className='border-b-2 border-black w-3/4'
-                        placeholder="Enter friend's ID here"
-                        type="number"
-                        onChange={R.pipe(R.pathOr('err',['target', 'value']), parseInt, setEnteredValue)}
-                    />
-                    <GoodButton onClick={() => onIdSubmit(enteredValue)}>Go!</GoodButton>
-                </div>
-            </div>
-        </StyledModal>)
+            </StyledModal>
+        )
+    )({id, onIdSubmit, gameStarted})
 }
 
 export default IdModal
