97c1b97691c2b9ddefa1f5393ef04fc268861758
Matthew John Froggatt <mattfr@live.ca>
2022-02-05 23:07:15 -0500

    Refactor SelectBox to use Ramda

--- src/Components/Board/SelectBox.tsx
+++ src/Components/Board/SelectBox.tsx
@@ -1,3 +1,4 @@
+import * as R from 'ramda'
 import React from "react";
 
 interface SelectBoxProps {
@@ -6,7 +7,7 @@ interface SelectBoxProps {
 }
 
 const SelectBox: React.FC<SelectBoxProps> = ({children, isSelected}) =>
-    <div className={isSelected? "bg-green-200 border-4 border-green-800 rounded-2xl p-1": "p-2"}>
+    <div className={R.ifElse(R.always(isSelected), R.always("bg-green-200 border-4 border-green-800 rounded-2xl p-1"), R.always("p-2"))()}>
         {children}
     </div>
 
