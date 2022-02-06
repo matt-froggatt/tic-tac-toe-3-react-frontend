25dc9490cb4aa3011f004d9f853bcb08d1b9cd17
Matthew John Froggatt <mattfr@live.ca>
2022-02-05 22:50:08 -0500

    Refactor IconFromPlayer to use Ramda

--- src/Components/Icons/IconFromPlayer.tsx
+++ src/Components/Icons/IconFromPlayer.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import IconX from "./IconX";
 import IconO from "./IconO";
 import {Player} from "../../gameRules";
+import * as R from "ramda";
 
 interface IconFromTextProps {
     player: Player
 }
 
-const IconMap = new Map([[Player.X, <IconX/>], [Player.O, <IconO/>]])
+const PLAYER: keyof IconFromTextProps = 'player'
 
-const IconFromPlayer: React.FC<IconFromTextProps> = ({player}) => {
-    return IconMap.get(player) || null
-}
+const IconFromPlayer: React.FC<IconFromTextProps> = R.cond([
+    [R.propEq(PLAYER, Player.O), R.always(<IconO/>)],
+    [R.propEq(PLAYER, Player.X), R.always(<IconX/>)],
+    [R.T, R.always(null)]
+])
 
 export default IconFromPlayer
