cee8c9e80cc3c9c494dc5b02e9d8aa4a47ec50e1
Matthew John Froggatt <mattfr@live.ca>
2022-02-08 02:34:16 -0500

    Updating gameRules to use Ramda WIP

--- src/gameRules.ts
+++ src/gameRules.ts
@@ -39,35 +39,20 @@ function generateArrayOfArraysOf<T>(itemCreator: () => T, width: number, height:
     return array
 }
 
-function generateStartInnerState(levels = 2, width = 3, height = 3): BoardState {
-    return {
-        winner: Player.NONE,
-        isPlayable: true,
-        containedItems: levels === 1 ? generateArrayOfArraysOf(() => Player.NONE, width, height) : generateArrayOfArraysOf(() => generateStartInnerState(levels - 1, width, height), width, height)
-    }
-}
+const generateStartInnerState = (levels = 2, width = 3, height = 3): BoardState => ({
+    winner: Player.NONE,
+    isPlayable: true,
+    containedItems: levels === 1 ? generateArrayOfArraysOf(() => Player.NONE, width, height) : generateArrayOfArraysOf(() => generateStartInnerState(levels - 1, width, height), width, height)
+})
 
-function generateStartState({levels = 2, width = 3, height = 3}): State {
-    return {
+const generateStartState = ({levels = 2, width = 3, height = 3}): State => ({
         winner: Player.NONE,
         turn: Player.X,
         board: generateStartInnerState(levels, width, height)
     }
-}
+)
 
 const startState = generateStartState({width: 3, height: 3})
-
-function first<T>(array: T[]): T {
-    return array[0];
-}
-
-function last<T>(array: T[]): T {
-    return array[array.length - 1];
-}
-
-function rest<T>(array: T[]): T[] {
-    return array.slice(1, array.length)
-}
 
 // TODO add typing
 function changeAtIndex(array: any[], value: any, index: number): any[] {
@@ -76,16 +61,14 @@ function changeAtIndex(array: any[], value: any, index: number): any[] {
     return newArray
 }
 
-function coordinatesFromArray(array: Coordinate[]): Coordinates {
-    return {data: array}
-}
+const coordinatesFromArray = (array: Coordinate[]): Coordinates => ({data: array})
 
 function changeAtCoordinates(coordinates: Coordinates, board: BoardState, change: Function): BoardState | false {
-    const firstCoord = first(coordinates.data);
+    const firstCoord = R.head(coordinates.data)!
     const cell = board.containedItems[firstCoord.x][firstCoord.y]
     let returnValue: BoardState | false = false
     if (isBoard(cell)) {
-        const tempCell = changeAtCoordinates(coordinatesFromArray(rest(coordinates.data)), cell, change)
+        const tempCell = changeAtCoordinates(coordinatesFromArray(R.tail(coordinates.data)), cell, change)
         if (tempCell) {
             returnValue = {
                 winner: board.winner,
@@ -116,19 +99,9 @@ function isArrayOfBoardArray(arrayOfArrays: any[][]): arrayOfArrays is BoardStat
     return true
 }
 
-function firstCoordinate(coordinates: Coordinates): Coordinate {
-    return first(coordinates.data)
-}
+const firstCoordinate = (coordinates: Coordinates): Coordinate => R.head(coordinates.data)!
 
-function restCoordinates(coordinates: Coordinates): Coordinates {
-    return {data: rest(coordinates.data)}
-}
-
-function coordinatesEq(c1: Coordinate, c2: Coordinate): boolean {
-    return c1.x === c2.x && c1.y === c2.y
-}
-
-// TODO make full everything playable when selected square is full
+const restCoordinates = (coordinates: Coordinates): Coordinates => ({data: R.tail(coordinates.data)})
 
 function isAnyEmpty(squares: Player[][]): boolean {
     for (const array of squares) {
@@ -143,7 +116,7 @@ function foldr<T, S>(array: T[], start: S, func: (item: T, value: S) => S): S {
     if (array.length === 0) {
         return start
     }
-    return func(first(array), foldr(rest(array), start, func))
+    return func(R.head(array)!, foldr(R.tail(array), start, func))
 }
 
 function isBoardFull(board: BoardState): boolean {
@@ -168,12 +141,11 @@ function getBoardAtCoordinates(coordinates: Coordinates, board: BoardState): Boa
         board : getBoardAtCoordinates(restCoordinates(coordinates), board.containedItems[firstCoord.x][firstCoord.y])
 }
 
-function isBoardAtCoordinatesFull(coordinates: Coordinates, state: State): boolean {
-    return isBoardFull(getBoardAtCoordinates(coordinates, getBoardFromState(state)))
-}
+const isBoardAtCoordinatesFull = (coordinates: Coordinates, state: State): boolean =>
+    isBoardFull(getBoardAtCoordinates(coordinates, getBoardFromState(state)))
 
 function updatePlayable(playableCoordinate: Coordinate, board: BoardState, isParentPlayable = false, currentCoordinates: Coordinates = EmptyCoordinates): BoardState {
-    const isPlayable = isParentPlayable || (firstCoordinate(currentCoordinates) && coordinatesEq(playableCoordinate, firstCoordinate(currentCoordinates)))
+    const isPlayable = isParentPlayable || (firstCoordinate(currentCoordinates) && R.equals(playableCoordinate, firstCoordinate(currentCoordinates)))
     return {
         winner: board.winner,
         isPlayable: isPlayable,
@@ -188,7 +160,7 @@ function updatePlayable(playableCoordinate: Coordinate, board: BoardState, isPar
     }
 }
 
-const getWinnerOfCell = (item: BoardState | Player): Player  => isBoard(item) ? winnerOfBoard(item.containedItems) : item
+const getWinnerOfCell = (item: BoardState | Player): Player => isBoard(item) ? winnerOfBoard(item.containedItems) : item
 
 function verticalWinner(board: BoardState[][] | Player[][], column: number): Player {
     let prevWinner = null
@@ -293,7 +265,7 @@ export function updateState(coordinates: Coordinates, state: State): State {
     const updatedState = {
         winner: newBoard && winnerUpdatedBoard ? winnerUpdatedBoard.winner : state.winner, // Check winners of InnerStates
         turn: newBoard ? state.turn === Player.X ? Player.O : Player.X : state.turn,
-        board: newBoard && winnerUpdatedBoard ? updatePlayable(last(coordinates.data), winnerUpdatedBoard, !isBoardAtCoordinatesFull({data: [last(coordinates.data)]}, state)) : state.board
+        board: newBoard && winnerUpdatedBoard ? updatePlayable(R.last(coordinates.data)!, winnerUpdatedBoard, !isBoardAtCoordinatesFull({data: [R.last(coordinates.data)!]}, state)) : state.board
     }
 
     console.log(updatedState)
